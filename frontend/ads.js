cbe8bbe39dd910f5829bd0a7f8d95d3e98de956d
mdlisan1029 <lisansarker002@gmail.com>
2026-06-22 23:29:14 +0600

    Refactor showRewardedAd function for clarity

--- frontend/ads.js
+++ frontend/ads.js
@@ -1,27 +1,5 @@
-async function showRewardedAd() {
+async function showRewardedAd(){
 
-alert("Calling Monetag");
-
-setTimeout(() => {
-alert("Still waiting...");
-}, 5000);
-
-try {
-
-let result = await show_11176239();
-
-alert("Ad Finished");
-
-return result;
-
-}
-
-catch(e){
-
-alert("Error");
-
-alert(JSON.stringify(e));
-
-}
+return show_11176239('pop');
 
 }
