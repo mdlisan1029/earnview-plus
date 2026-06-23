49d340e5ee8ab065acbc3e944ba24f5939b08963
mdlisan1029 <lisansarker002@gmail.com>
2026-06-23 14:53:18 +0600

    Refactor balance and adsToday initialization logic

--- frontend/script.js
+++ frontend/script.js
@@ -1,84 +1,27 @@
-let balance =
-Number(localStorage.getItem("balance"))
-|| 0;
-
-
-let adsToday =
-Number(localStorage.getItem("adsToday"))
-|| 0;
-
-
-updateUI();
-
+let balance = 0;
+let adsToday = 0;
 
 
 async function watchAd(){
 
-
-if(adsToday>=100){
-
-return;
-
-}
-
-
 try{
-
 
 await showRewardedAd();
 
-
 adsToday++;
 
-
-localStorage.setItem(
-
-"adsToday",
-
-adsToday
-
-);
-
-
-updateUI();
-
+document.getElementById("ads").innerText = adsToday;
 
 }
-
 
 catch(e){
 
-
 console.log(e);
 
+}
 
 }
 
 
-}
-
-
-
-function updateUI(){
-
-
-document.getElementById(
-
-"balance"
-
-).innerText=
-
+document.getElementById("balance").innerText =
 balance.toFixed(2);
-
-
-
-document.getElementById(
-
-"ads"
-
-).innerText=
-
-adsToday;
-
-
-}
